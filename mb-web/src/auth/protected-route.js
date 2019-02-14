707bb88590b9c88435afb74512ad048b042354de
Natcha Luangaroonchai <natcha.luangaroonchai@krungthai-axa.co.th>
2019-02-14 17:44:07 +0700

    Update protected-route.js

--- mb-web/src/auth/protected-route.js
+++ mb-web/src/auth/protected-route.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const ProtectedRoute = (props) => {
   if (!props.authService.isAuthenticated()) {
-    props.history.replace('/login');
+    // Redirect to the Auth0 which will store current pathname to the localStorage.
+    // The current pathname will be used once the user has been logged in successfully.
+    props.authService.login(props.location.pathname);
     
     return null;
   }
@@ -10,6 +13,14 @@ const ProtectedRoute = (props) => {
   return (
     <props.component { ...props } />
   );
+};
+
+ProtectedRoute.propTypes = {
+  authService: PropTypes.object.isRequired,
+  component: PropTypes.node.isRequired,
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default ProtectedRoute;
