e6abb2476065d67b6c4e5462a5a4836033ceacc1
Natcha Luangaroonchai <me@nomkhonwaan.com>
2019-02-18 21:08:31 +0700

    [LINE] Temporary disable LINE notify service

--- mb-notification/src/main/kotlin/com/nomkhonwaan/mb/notification/NotificationEventListener.kt
+++ mb-notification/src/main/kotlin/com/nomkhonwaan/mb/notification/NotificationEventListener.kt
@@ -16,55 +16,55 @@ import org.springframework.stereotype.Component
 @Component
 @EnableAutoConfiguration
 class NotificationEventListener(private val lineNotifyService: LINENotifyService) {
-    /**
-     * Sends a text message when the new Post has been created.
-     *
-     * @param event A Post created Event
-     */
-    @EventHandler
-    fun on(event: PostCreatedEvent) {
-        lineNotifyService
-                .notify("""
-                    A new Post has been created by [${event.authorId}]: https://www.nomkhonwaan.com/dashboard/posts/${event.id}
-                """.trimIndent()).subscribe()
-    }
+//    /**
+//     * Sends a text message when the new Post has been created.
+//     *
+//     * @param event A Post created Event
+//     */
+//    @EventHandler
+//    fun on(event: PostCreatedEvent) {
+//        lineNotifyService
+//                .notify("""
+//                    A new Post has been created by [${event.authorId}]: https://www.nomkhonwaan.com/dashboard/posts/${event.id}
+//                """.trimIndent()).subscribe()
+//    }
 
-    /**
-     * Sends a text message when the Post title has been updated.
-     *
-     * @param event A Post title updated Event
-     */
-    @EventHandler
-    fun on(event: PostTitleUpdatedEvent) {
-        lineNotifyService
-                .notify("""
-                    The Post [${event.id}] title has been updated to [${event.title}]: https://www.nomkhonwaan.com/dashboard/posts/${event.id}
-                """.trimIndent()).subscribe()
-    }
+//    /**
+//     * Sends a text message when the Post title has been updated.
+//     *
+//     * @param event A Post title updated Event
+//     */
+//    @EventHandler
+//    fun on(event: PostTitleUpdatedEvent) {
+//        lineNotifyService
+//                .notify("""
+//                    The Post [${event.id}] title has been updated to [${event.title}]: https://www.nomkhonwaan.com/dashboard/posts/${event.id}
+//                """.trimIndent()).subscribe()
+//    }
 
-    /**
-     * Sends a text message when the Post status has been updated.
-     *
-     * @param event A Post status updated Event
-     */
-    @EventHandler
-    fun on(event: PostStatusUpdatedEvent) {
-        lineNotifyService
-                .notify("""
-                    The Post [${event.id}] status has been updated to [${event.status}]: https://www.nomkhonwaan.com/dashboard/posts/${event.id}
-                """.trimIndent()).subscribe()
-    }
+//    /**
+//     * Sends a text message when the Post status has been updated.
+//     *
+//     * @param event A Post status updated Event
+//     */
+//    @EventHandler
+//    fun on(event: PostStatusUpdatedEvent) {
+//        lineNotifyService
+//                .notify("""
+//                    The Post [${event.id}] status has been updated to [${event.status}]: https://www.nomkhonwaan.com/dashboard/posts/${event.id}
+//                """.trimIndent()).subscribe()
+//    }
 
-    /**
-     * Sends a text message when the Post content has been updated.
-     *
-     * @param event A Post content updated Event
-     */
-    @EventHandler
-    fun on(event: PostContentUpdatedEvent) {
-        lineNotifyService
-                .notify("""
-                    The Post [${event.id}] content has been updated: https://www.nomkhonwaan.com/dashboard/posts/${event.id}
-                """.trimIndent()).subscribe()
-    }
+//    /**
+//     * Sends a text message when the Post content has been updated.
+//     *
+//     * @param event A Post content updated Event
+//     */
+//    @EventHandler
+//    fun on(event: PostContentUpdatedEvent) {
+//        lineNotifyService
+//                .notify("""
+//                    The Post [${event.id}] content has been updated: https://www.nomkhonwaan.com/dashboard/posts/${event.id}
+//                """.trimIndent()).subscribe()
+//    }
 }
