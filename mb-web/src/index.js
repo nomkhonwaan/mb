1fff75566bd824c1c3f52be06054543763be8ae6
Natcha Luangaroonchai <me@nomkhonwaan.com>
2019-02-20 20:10:03 +0700

    Just remove unused comments

--- mb-web/src/index.js
+++ mb-web/src/index.js
@@ -6,8 +6,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-// import { ApolloProvider } from 'react-apollo';
-// import ApolloClient from 'apollo-boost';
 
 /**
  * Internal Dependencies
@@ -29,18 +27,6 @@ const authService = AuthService.Builder
   .withClientId(process.env.REACT_APP_AUTH0_CLIENT_ID)
   .withRedirectUri(process.env.REACT_APP_AUTH0_REDIRECT_URI)
   .build();
-
-/* GraphQL */
-// const configOptions = { uri: process.env.REACT_APP_GRAPHQL_ENDPOINT };
-
-// // Set access token to the request header if logged in
-// if (authService.isAuthenticated()) {
-//   Object.assign(configOptions, {
-//     headers: { authorization: `Bearer ${authService.getAccessToken()}`, },
-//   });
-// }
-
-// const client = new ApolloClient(configOptions);
 
 /* Redux */
 const store = configureStore(apiClient, authService);
