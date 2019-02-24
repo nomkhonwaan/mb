0a6b5a0ed822f31f0cc8b05f95b5e4e0451257e2
Natcha Luangaroonchai <me@nomkhonwaan.com>
2019-02-24 16:26:00 +0700

    Style medium Post

--- mb-web/src/components/post/medium-post.js
+++ mb-web/src/components/post/medium-post.js
@@ -15,34 +15,44 @@ import PropTypes from 'prop-types';
  */
 const MediumPost = (props) => {
   return (
-    <div className="medium-post">
-      <h2 className="title">{ props.title }</h2>
+    <div className="post -medium">
+      <h2 className="title">
+        <Link to={ `/${moment(props.publishedAt).format('YYYY/MM/DD')}/${props.slug}` }>{ props.title }</Link>
+      </h2>
 
       <p
         className="content" 
         dangerouslySetInnerHTML={ { __html: /<p>(.+?)<\/p>/.exec(props.html)[1] } }
       />
 
-      <div className="metadata">
+      <div className="footer">
         <img
           alt={ props.author.displayName } 
           className="author-avatar"
           src={ props.author.avatarUrl } 
         />
-        <div className="author-display-name">{ props.author.displayName }</div>
-        <time 
-          className="published-at"
-          dateTime={ props.publishedAt }
-        >
-          { moment(props.publishedAt).format('LL') }
-        </time>
-        on 
-        <div className="categories">
-          {
-            props.categories.map((category, key) => 
-              <Link key={ key } to={ `/categories/${category.slug}` }>{ category.name }</Link>
-            )
-          }
+
+        <div className="author-display-name _flex _flex-vertical-align-middle">
+          { props.author.displayName }
+        </div>
+        
+        <div className="metadata _flex _flex-vertical-align-middle">
+          <time 
+            className="published-at"
+            dateTime={ props.publishedAt }
+          >
+            { moment(props.publishedAt).format('LL') }
+          </time>
+
+          &nbsp;on&nbsp;
+
+          <span className="categories">
+            {
+              props.categories.map((category, key) => 
+                <Link key={ key } to={ `/categories/${category.slug}` }>{ category.name }</Link>
+              )
+            }
+          </span>
         </div>
       </div>
     </div>
