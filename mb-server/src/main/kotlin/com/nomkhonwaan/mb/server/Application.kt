ddcf32d8b015f2b2259a215f3bb1fafff74c5e66
Natcha Luangaroonchai <natcha.luangaroonchai@krungthai-axa.co.th>
2019-03-01 13:33:17 +0700

    Include mb-auth to the mb-server

--- mb-server/src/main/kotlin/com/nomkhonwaan/mb/server/Application.kt
+++ mb-server/src/main/kotlin/com/nomkhonwaan/mb/server/Application.kt
@@ -5,13 +5,14 @@ import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
 
 @ComponentScan(basePackages = [
-    "com.nomkhonwaan.mb.autoconfigure",
     "com.nomkhonwaan.mb.auth",
+    "com.nomkhonwaan.mb.autoconfigure",
     "com.nomkhonwaan.mb.blog",
     "com.nomkhonwaan.mb.chatbot",
     "com.nomkhonwaan.mb.graphql",
     "com.nomkhonwaan.mb.notification",
-    "com.nomkhonwaan.mb.restful"
+    "com.nomkhonwaan.mb.restful",
+    "com.nomkhonwaan.mb.storage"
 ])
 @SpringBootApplication
 class Application
