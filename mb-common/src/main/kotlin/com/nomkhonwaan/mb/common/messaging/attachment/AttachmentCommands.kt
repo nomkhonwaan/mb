7480fc215ad078af014c7e5fec67430d8c6d873c
Natcha Luangaroonchai <natcha.luangaroonchai@krungthai-axa.co.th>
2019-03-07 06:46:45 +0700

    Update AttachmentCommands.kt

--- mb-common/src/main/kotlin/com/nomkhonwaan/mb/common/messaging/attachment/AttachmentCommands.kt
+++ mb-common/src/main/kotlin/com/nomkhonwaan/mb/common/messaging/attachment/AttachmentCommands.kt
@@ -8,6 +8,7 @@ import java.io.InputStream
  *
  * @param id          An identifier of the Attachment
  * @param inputStream An uploaded file input stream
+ * @param size        A size of the uploaded file
  * @param path        A path that the file will be uploaded to
  */
 data class UploadFileCommand(
@@ -27,7 +28,7 @@ data class CompleteFileUploadingCommand(override val id: String) : Command<Strin
 /**
  * A rollback file uploading Command.
  * <p>
- * This command for rollback / delete uploaded file in the storage server in case of failure.
+ * This command for rollback uploaded file in the storage server in case of failure.
  *
  * @param id   An identifier of the Attachment
  * @param path A path that the file belongs to
