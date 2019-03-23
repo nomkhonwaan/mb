59b55039f78942c324acd0794728a56b2f489a02
Natcha Luangaroonchai <me@nomkhonwaan.com>
2019-03-23 23:20:31 +0700

    [WIP] Add more shareable components

--- mb-web/src/app/shared/shared.module.ts
+++ mb-web/src/app/shared/shared.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
+import { SlideToggleComponent } from './slide-toggle/slide-toggle.component';
+import { DialogComponent } from './dialog/dialog.component';
+import { DropdownComponent } from './dropdown/dropdown.component';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ButtonComponent],
-  exports: [ButtonComponent]
+  declarations: [ButtonComponent, SlideToggleComponent, DialogComponent, DropdownComponent, InputComponent],
+  exports: [ButtonComponent, SlideToggleComponent, DialogComponent, DropdownComponent, InputComponent],
 })
 export class SharedModule { }
