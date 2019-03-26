0db2ea7b91c822f243e6aa1bbddf4f925f721a68
Natcha Luangaroonchai <natcha.luangaroonchai@krungthai-axa.co.th>
2019-03-26 16:51:29 +0700

    [WIP] :D

--- mb-web/src/app/shared/dialog/dialog.component.ts
+++ mb-web/src/app/shared/dialog/dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, OnDestroy, HostBinding } from '@angular/core';
 import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
@@ -7,7 +7,7 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
   styleUrls: ['./dialog.component.scss'],
   animations: [
     trigger('fadeInOut', [
-      transition(':enter', [
+      transition('* => true', [
         style({ opacity: 0 }),
         animate('.4s ease-in-out')
       ]),
@@ -17,7 +17,7 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
     ])
   ]
 })
-export class DialogComponent implements OnInit {
+export class DialogComponent implements OnInit, OnDestroy {
 
   /**
    * A callback function that fires after backdrop has been closed.
@@ -28,18 +28,28 @@ export class DialogComponent implements OnInit {
   /**
    * A state for triggering :enter and :leave transitions.
    */
+  @HostBinding('@fadeInOut')
   show: boolean;
 
-  constructor() { }
-
   ngOnInit(): void {
-    // NOTE: For triger on :enter transition.
-    // I'm not sure why assigning value directly doesn't work but in async context works.
-    setTimeout(() => { this.show = true }, 0);
+    // NOTE:
+    // For trigger fade-in transition. I'm not sure why direct assign value doesn't work.
+    setTimeout(() => this.show = true, 0);
   }
 
-  close(): void {
-    this.onClose.emit();
+  ngOnDestroy(): void {
+
   }
+
+  // ngOnDestroy(): void {
+  //   console.log('test');
+  // }
+
+  // close(): void {
+  //   setTimeout(() => {
+  //     this.show = false;
+  //     // this.onClose.emit();
+  //   }, 0);
+  // }
 
 }
