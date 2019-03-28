2739887a4f496db21e41cfb935d1a0e2e5f09fdb
Natcha Luangaroonchai <natcha.luangaroonchai@krungthai-axa.co.th>
2019-03-28 10:42:56 +0700

    Finally, remove all fade-in-out animation

--- mb-web/src/app/shared/dialog/dialog.component.ts
+++ mb-web/src/app/shared/dialog/dialog.component.ts
@@ -1,41 +1,24 @@
-import { Component, EventEmitter, OnInit, Output, OnDestroy, HostBinding } from '@angular/core';
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
-  selector: 'app-dialog',
-  templateUrl: './dialog.component.html',
-  styleUrls: ['./dialog.component.scss'],
-  animations: [
-    trigger('fadeInOut', [
-      state('false', style({ opacity: 0 })),
-      transition('* => true', [
-        style({ opacity: 0 }),
-        animate('.4s ease-in-out')
-      ]),
-      transition('true => false',
-        animate('.4s ease-in-out', style({ opacity: 0 }))
-      )
-    ])
-  ]
+  selector: "app-dialog",
+  templateUrl: "./dialog.component.html",
+  styleUrls: ["./dialog.component.scss"],
+  host: {
+    "(click)": "close()"
+  }
 })
-export class DialogComponent implements OnInit {
-
+export class DialogComponent {
   /**
-   * A callback function that fires after backdrop has been closed.
+   * A callback function that fires on dialog close event.
    */
   @Output()
   onClose: EventEmitter<null> = new EventEmitter();
 
   /**
-   * A state for triggering :enter and :leave transitions.
+   * Fires dialog close event.
    */
-  @HostBinding('@fadeInOut')
-  show: boolean;
-
-  ngOnInit(): void {
-    // NOTE:
-    // For trigger fade-in transition. I'm not sure why direct assign value doesn't work.
-    setTimeout(() => this.show = true, 0);
+  close() {
+    this.onClose.emit();
   }
-
 }
