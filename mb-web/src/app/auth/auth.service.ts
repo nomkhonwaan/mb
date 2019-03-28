c5aa7be97d99546126bd65029a4a7115989d03b7
Natcha Luangaroonchai <natcha.luangaroonchai@krungthai-axa.co.th>
2019-03-28 16:56:06 +0700

    Remove builder pattern, will use DI pattern instead

--- mb-web/src/app/auth/auth.service.ts
+++ mb-web/src/app/auth/auth.service.ts
@@ -1,41 +1,12 @@
-import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Injectable()
 export class AuthService {
-
   private idToken: string;
   private accessToken: string;
   private expiresAt: number;
 
-  constructor(public router: Router) {
-
-  }
-
-  // static get Builder() {
-  //   class Builder {
-  //     domain: string = 'nomkhonwaan.auth0.com';
-  //     clientId = '';
-  //     responseType = 'token id_token';
-  //     redirectUri = '';
-  //     scope = 'openid email profile';
-
-  //     withClientId(clientId) {
-  //       this.clientId = clientId;
-  //       return this;
-  //     }
-
-  //     withRedirectUri(redirectUri) {
-  //       this.redirectUri = redirectUri;
-  //       return this;
-  //     }
-
-  //     build() {
-  //       return new AuthService()
-  //     }
-  //   }
-
-  //   return new Builder();
-  // }
+  constructor(public router: Router) {}
 
 }
